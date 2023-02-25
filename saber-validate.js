424782c5e156bb297b950a3cf65f8cb8066a7c9b
Ahmed Saber <ahmed0saber33@gmail.com>
2023-02-25 13:51:03 +0200

    check if only_spaces is working

--- saber-validate.js
+++ saber-validate.js
@@ -7,7 +7,6 @@ const allValidationRules = {
   },
   only_spaces: (input) => {
     if (input.value.length > 0 && input.value.trim().length === 0) {
-      console.log(input.value.length === 0);
       return true;
     }
     return false;
@@ -38,13 +37,4 @@ HTMLInputElement.prototype.checkValidation = function (rules, options) {
   });
 
   console.log(errors);
-
-  // console.log(allValidationRules["empty"](currentInput))
-  // console.log(allValidationRules["only_spaces"](currentInput))
-
-  // console.log({
-  //     currentInput,
-  //     rules,
-  //     options
-  // })
 };
