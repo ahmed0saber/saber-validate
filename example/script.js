036ab743b6c13a09c8eb3e99f903f5a7d6154d55
esraa00 <em4728644@gmail.com>
2023-03-11 23:25:53 +0200

    added max and min to validation rules

--- example/script.js
+++ example/script.js
@@ -1,21 +1,24 @@
 const emailValidationRules = [
-    ["empty", "email is required"],
-    ["only_spaces", "email can't be only spaces"],
-    ["invalid_email", "email is not valid"]
-]
-const emailValidationOptions = {
-    method: "field", // console, alert or field
-    block: true // block after the first error (don't show all errors, just the first one)
-}
+  ["empty", "email is required"],
+  ["only_spaces", "email can't be only spaces"],
+  ["invalid_email", "email is not valid"],
+  ["max-8", "shouldn't be greater than 8 characters"],
+  ["min-3", "shouldn't be less than 3 characters"],
+];
 
-const emailInput = document.querySelector(".email-input")
-const registerForm = document.querySelector(".register-form")
+const emailValidationOptions = {
+  method: "field", // console, alert or field
+  block: false, // block after the first error (don't show all errors, just the first one)
+};
+
+const emailInput = document.querySelector(".email-input");
+const registerForm = document.querySelector(".register-form");
 
 const validateEmailAddress = () => {
-    emailInput.checkValidation(emailValidationRules, emailValidationOptions)
-}
+  emailInput.checkValidation(emailValidationRules, emailValidationOptions);
+};
 
-emailInput.addEventListener("keyup", validateEmailAddress)
+emailInput.addEventListener("keyup", validateEmailAddress);
 registerForm.addEventListener("submit", () => {
-    validateEmailAddress()
-})
+  validateEmailAddress();
+});
